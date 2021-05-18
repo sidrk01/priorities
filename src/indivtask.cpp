c78d8650b5c6fc21d37d6a91d7bd4be499e2669a
cheyenneayyoub <cayyo001@ucr.edu>
2021-05-18 11:43:50 -0700

    Implimented modifyTask

--- src/indivtask.cpp
+++ src/indivtask.cpp
@@ -1,10 +1,27 @@
 #include "indivtask.hpp"
-
+#include <vector>
+#include <sstream>
 using namespace std;
 
 int IndivTask::nextId = 1;
 
 IndivTask::IndivTask(string daysOfWeek, bool recurring, string title, string group) {
+	//enter days with commas separating them "m,t,w"
+	/*
+	if(daysOfWeek.find(",")){
+		vector<string> output;
+        	stringstream s(daysOfWeek);
+        	while(s.good()){
+                	string substr;
+                	getline(s, substr, ',');
+                	output.push_back(substr);
+        	}
+        	for(int i = 0; i < output.size(); ++i){
+                	this->daysOfWeek = output.at(i); //overwrites the day
+        	}
+	}	
+	*/
+
 	this->daysOfWeek = daysOfWeek;
 	this->recurring = recurring;
 	this->title = title;
@@ -26,4 +43,11 @@ void IndivTask::remove(int id) {
 
 string IndivTask::printToDo() {
 	
+}
+
+void IndivTask::modifyTask(string daysOfWeek, bool recurring, string title, string group){		
+	this->daysOfWeek = daysOfWeek;
+        this->recurring = recurring;
+        this->title = title;
+        this->group = group;		
 }
