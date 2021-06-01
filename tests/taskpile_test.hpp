0f90b7d92c45b8d67e0241a646e95b74c7c6e61c
cheyenneayyoub <cayyo001@ucr.edu>
2021-05-31 20:07:33 -0700

    working tests

--- tests/taskpile_test.hpp
+++ tests/taskpile_test.hpp
@@ -1,8 +1,8 @@
 #ifndef __TASKPILE_TEST_HPP__
-#define __TASKPILE_TEST_HPP
-#include "../header/task.hpp"
-#include "../header/indivtask.hpp"
-#include "../header/taskpile.hpp"
+#define __TASKPILE_TEST_HPP__
+//#include "../header/task.hpp"
+//#include "../src/indivtask.cpp"
+#include "../src/taskpile.cpp"
 #include "gtest/gtest.h"
 #include <string>
 
@@ -11,7 +11,7 @@ using namespace std;
 TEST(TaskPile, AddTaskPile){
 
 	string s = "school";
-	Task* yee = new IndivTask("yyyyyyy",s);
+	Task* yee = new IndivTask("yyyyyyy","task");
 	TaskPile* hi = new TaskPile(s);
 	hi->add_task(yee);
 	
@@ -19,18 +19,45 @@ TEST(TaskPile, AddTaskPile){
 }
 
 TEST(TaskPile, TaskInTask){
-	
+	string s = "school";
+        Task* yee = new IndivTask("yyyyyyy","task");
+        TaskPile* hi = new TaskPile(s);
+        TaskPile* wat = new TaskPile("CS");
+	hi->add_task(yee);
+	wat->add_task(hi);
+	EXPECT_EQ(wat->title, "CS");
 }
 
 TEST(TaskPile, RemoveTaskPile){
-
+	string s = "school";
+        Task* yee = new IndivTask("yyyyyyy","task");
+        TaskPile* hi = new TaskPile(s);
+        TaskPile* wat = new TaskPile("CS");
+        hi->add_task(yee);
+        wat->add_task(hi);
+	//cout << wat->id; 12
+	wat->removeTask(wat->id);
+	EXPECT_EQ(hi->title, "school");
 }
 
 TEST(TaskPile, ModifyTaskPile){
+	string s = "school";
+        Task* yee = new IndivTask("yyyyyyy","task");
+        TaskPile* hi = new TaskPile(s);
+        hi->add_task(yee);
+	//cout << hi->id; 14
+	hi->modifyTask("nnnnnnn","hello",14);
 
+        EXPECT_EQ(hi->title, "hello");	
 }
 
 TEST(TaskPile, MarkAsComplete){
+	string s = "school";
+	Task* yee = new IndivTask("yyyyyyy","task");
+	TaskPile* hi = new TaskPile(s);
+	hi->markComplete(16);
+	//cout << hi->id;
+	EXPECT_EQ(hi->complete, true);
 
 }
 
