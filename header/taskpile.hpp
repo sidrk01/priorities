8e5998ca458da85e84766335a98b8a5b9d3c1f80
cheyenneayyoub <cayyo001@ucr.edu>
2021-05-31 17:58:58 -0700

    updated

--- header/taskpile.hpp
+++ header/taskpile.hpp
@@ -8,15 +8,14 @@ using namespace std;
 
 
 class TaskPile : public Task {
-	private:
+	public:
 		vector<Task*> tasks;
 		string name;
-	public:
 		virtual void markComplete();
 		TaskPile(string title);
-		virtual void modifyTask(const string& dow, string title);
-		// virtual void remove(); //could be a function of task pile but would need an id
-		virtual void addTask(const string& dow, string title);
+		virtual void modifyTask(const string& dow, string title, int id);
+		virtual void remove(int id); //could be a function of task pile but would need an id
+		void add_task(Task* task);
 		~TaskPile();
 
 };
